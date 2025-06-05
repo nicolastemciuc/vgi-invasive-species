5b1877b445fdde477eb4c8409fdcc3ddf274f87e
joaquin tomas <joaquintomas2003@gmail.com>
2025-06-05 19:11:11 -0300

    Add `create` action for `sightings`

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,4 +1,6 @@
 class SightingsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
+
   def index
   end
 
@@ -7,5 +9,22 @@ class SightingsController < ApplicationController
       latitude: params[:lat],
       longitude: params[:lng]
     )
+  end
+
+  def create
+    @sighting = current_user.sightings.build(sighting_params)
+    @sighting.status = "Pendiente"
+
+    if @sighting.save
+      redirect_to sightings_path, notice: "Avistamiento creado exitosamente"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def sighting_params
+    params.expect(sighting: [ :latitude, :longitude, :location_desc, :description, :sighting_date ])
   end
 end
