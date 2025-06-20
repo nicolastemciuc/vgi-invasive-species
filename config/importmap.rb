97326a3f0b49552079b8e6284ed291cfa28d40d6
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-06-20 00:35:50 -0300

    chore: run `bin/importmap pin @geoman-io--leaflet-geoman-free`

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "leaflet" # @1.9.4
+pin "@geoman-io/leaflet-geoman-free", to: "@geoman-io--leaflet-geoman-free.js" # @2.18.3
